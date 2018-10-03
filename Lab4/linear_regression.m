b8b99f68cd5dd20bc3ea554425831aabc70968bb
lucas420179 <lucas420179@gmail.com>
2018-10-03 14:46:23 -0300

    alterei o arquivo para rodar no matlab/octave

--- Lab4/linear_regression.m
+++ Lab4/linear_regression.m
@@ -13,19 +13,19 @@ function [coef] = linear_regression (x, y,opt)
         comand = opt;
   end
   %Imprime mensagem de erro para parâmetros de dimensões diferentes
-  if length_x != length_y
-    disp("Fatal error: Matrizes do parâmetro possuem tamanhos diferentes")
+  if length_x ~= length_y
+    disp('Fatal error: Matrizes do parâmetro possuem tamanhos diferentes')
   else
     array_1 = ones(length_x,1);     %Matriz de 1's para determinar coeficiente ang
     X = transpose(x);               %Correção da matriz para matriz coluna
     Y = transpose(y);               %Correção da matriz para matriz coluna
     X = array_1 * [1 0] + X*[0 1];  %Cria a matriz corrigida
     coef = inv(transpose(X)*X)*transpose(X)*Y;    
-    if comand == "verbose"
-      disp("coef(1) = coeficiente linear");
-      disp("coef(2) = coeficiente angular");
+    if comand == 'verbose'
+      disp('coef(1) = coeficiente linear');
+      disp('coef(2) = coeficiente angular');
     end
   end
     
-endfunction
+end
 
